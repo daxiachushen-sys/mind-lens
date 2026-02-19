bb3911caac5938eb40d1a71c9456daaa3c624312
daxiachushen-sys <daxiachushen@gmail.com>
2026-02-19 18:01:34 +0800

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  optimizeFonts: false,
-};
-export default nextConfig;
